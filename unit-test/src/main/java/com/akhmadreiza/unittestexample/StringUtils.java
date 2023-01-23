4eace9d6083af1352e7e25852aa0fd79d2508a52
akhmadreiza <reizaarmando@gmail.com>
2023-01-23 22:05:48 +0700

    update for youtube vid

--- unit-test/src/main/java/com/akhmadreiza/unittestexample/StringUtils.java
+++ unit-test/src/main/java/com/akhmadreiza/unittestexample/StringUtils.java
@@ -4,4 +4,11 @@ public class StringUtils {
     public static boolean isEmpty(String str) {
         return str == null || "".equals(str);
     }
+
+    public static String[] splitString(String inputString) {
+        if (isEmpty(inputString)) {
+            throw new IllegalStateException("input string cannot be null");
+        }
+        return inputString.split(" ");
+    }
 }
--- unit-test/src/test/java/com/akhmadreiza/unittestexample/StringUtilsTest.java
+++ unit-test/src/test/java/com/akhmadreiza/unittestexample/StringUtilsTest.java
@@ -43,4 +43,62 @@ public class StringUtilsTest {
         //assertion
         Assert.assertTrue(isEmpty);
     }
+
+    //Apabila dimasukkan kata-kata dengan spasi, maka akan return array sebanyak kata yang
+    //dipisahkan dengan spasi
+    @Test
+    public void testStringInputSplit() {
+        //prepare data
+        String inputString = "saya membuat unit test";
+
+        //do test
+        String[] actual = StringUtils.splitString(inputString);
+
+        //assertion
+        //assert length dari array actual adalah 4
+        System.out.println("input string length = " + actual.length);
+        Assert.assertEquals(4, actual.length);
+
+        //assert index 0 dari array actual adalah "saya"
+        System.out.println("actual[0] = " + actual[0]);
+        Assert.assertEquals("saya", actual[0]);
+
+        //assert index 1 dari array actual adalah "membuat"
+        System.out.println("actual[1] = " + actual[1]);
+        Assert.assertEquals("membuat", actual[1]);
+    }
+
+    @Test
+    public void testInputNullSplitString() {
+        //apabila method di input null, maka akan throw null pointer exception (NPE)
+
+        //prepare data
+        String input = null;
+
+        //do test
+        try {
+            String[] actual = StringUtils.splitString(input);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalStateException);
+            IllegalStateException ie = (IllegalStateException) e;
+            Assert.assertEquals("input string cannot be null", ie.getMessage());
+        }
+    }
+
+    @Test
+    public void testInputEmptyStringSplitString() {
+        //apabila method di input null, maka akan throw null pointer exception (NPE)
+
+        //prepare data
+        String input = "";
+
+        //do test
+        try {
+            String[] actual = StringUtils.splitString(input);
+        } catch (Exception e) {
+            Assert.assertTrue(e instanceof IllegalStateException);
+            IllegalStateException ie = (IllegalStateException) e;
+            Assert.assertEquals("input string cannot be null", ie.getMessage());
+        }
+    }
 }
